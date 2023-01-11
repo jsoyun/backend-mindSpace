df1135f19faaf0548d79cde245e9e3264876e4b1
soyun <syusmm233@gmail.com>
2023-01-11 23:27:49 +0900

    get, post 테스트

--- App.ts
+++ App.ts
@@ -7,6 +7,22 @@ app.set('port',process.env.PORT || 3000)
 app.get('/',(req:any,res:any)=>{
     res.send('헬로 지성아아아')
 })
+app.get('/test',(req:any,res:any)=>{
+    res.status(200).json({
+        "message":"테스트입니다"
+    })
+})
+
+
+
+
+app.post('/post',(req:any,res:any)=>{
+    console.log("req",req)
+    res.status(200).json({
+        "Dfdf":"어서와아"
+    })
+})
+
 
 
 app.listen(app.get('port'),()=>{
